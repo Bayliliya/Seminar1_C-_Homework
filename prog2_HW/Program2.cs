24a8146915bec629bfaf426c4db1c023df5b4a60
Liliya <gizat.83@mail.ru>
2022-05-26 20:27:50 +0300

    Task 2

---
+++ prog2_HW/Program2.cs
@@ -0,0 +1,18 @@
+﻿// Нахождение минимума -максимума из 2-х чисел
+Console.Write("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (b > a)
+{
+    Console.WriteLine("max = " + b);
+}
+else if (b == a)
+{
+
+    Console.WriteLine("a = b = " + a);
+}
+else
+{
+    Console.WriteLine("max = " + a);
+}
