35236795a206aa131da37c6ff18b7ff0f994b714
Liliya <gizat.83@mail.ru>
2022-05-30 11:18:42 +0300

    Задача на вывод четных чисел до N

---
+++ prog8_HW/Program8.cs
@@ -0,0 +1,17 @@
+﻿// Все четные числа от 1 до N
+Console.Write("Введите число N (больше единицы): ");
+int N = Convert.ToInt32(Console.ReadLine());
+int a = 2;
+if (N <= 1)
+{
+    Console.WriteLine("Ошибка ввода");
+}
+else
+{
+    while (a <= N)
+    {
+        Console.Write(a + " ");
+        a += 2;
+    }
+
+}
