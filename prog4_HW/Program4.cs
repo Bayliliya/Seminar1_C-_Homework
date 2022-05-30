b7fca789737455958ac9157f65e095d006d91816
Liliya <gizat.83@mail.ru>
2022-05-30 10:51:17 +0300

    Доработанная программа

--- prog4_HW/Program4.cs
+++ prog4_HW/Program4.cs
@@ -6,14 +6,22 @@ int b = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число с: ");
 int c = Convert.ToInt32(Console.ReadLine());
 int max = a;
-
-if (b >= max)
+if (a == b && b == c)
 {
-    max = b;
+    Console.WriteLine("Все числа равны  " + a);
 }
-else if (c >= max)
+else
 {
-    max = c;
-}
 
-Console.WriteLine("max = " + max);
+    if (b >= max)
+    {
+        max = b;
+    }
+    if (c >= max)
+    {
+        max = c;
+    }
+
+
+    Console.WriteLine(max);
+}
